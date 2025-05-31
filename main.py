f543c5828335eb6601c80bed7a01e8602fa87203
Yusuke Shirai <Shirai.Yusuke@gmail.com>
2025-05-31 16:15:56 +0900

    Added stream_mode

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import asyncio
 import os
 from dotenv import load_dotenv
+from langchain_core.messages.base import BaseMessage
 from langgraph.prebuilt import create_react_agent
 from langchain_mcp_adapters.tools import load_mcp_tools
 from mcp import ClientSession, StdioServerParameters, stdio_client
 
 load_dotenv(override=True)
+
 
 class BrowserAgent:
     def __init__(self, debug=False):
@@ -32,17 +34,37 @@ class BrowserAgent:
         if self.client:
             await self.client.__aexit__(exc_type, exc_val, exc_tb)
 
-    async def send_message(self, message: str):
+    def send_message(self, message: str, stream_mode: str = "values"):
         if not self.agent:
-            raise RuntimeError("Agent not initialized. Use 'async with' context manager.")
-        return self.agent.astream({"messages": [{"role": "user", "content": message}]}, debug=self.debug, stream_mode="values")
+            raise RuntimeError(
+                "Agent not initialized. Use 'async with' context manager."
+            )
+        return self.agent.astream(
+            {"messages": [{"role": "user", "content": message}]},
+            debug=self.debug,
+            stream_mode=stream_mode,
+        )
 
 
 if __name__ == "__main__":
+
     async def main():
         async with BrowserAgent(debug=False) as agent:
-            async for chunk in await agent.send_message("Yahoo! JAPANを開いてください"):
-                print(chunk)
+            # async for message_chunk, _ in agent.send_message(
+            #     "Googleを開いてください", stream_mode="messages"
+            # ):
+            #     if message_chunk.content:
+            #         print(message_chunk.content, end="|", flush=True)
+            async for chunk in agent.send_message(
+                "Googleを開いてください", stream_mode="updates"
+            ):
+                for _, v in chunk.items():
+                    if "messages" in v:
+                        for m in v["messages"]:
+                            if isinstance(m, BaseMessage):
+                                print(m.pretty_repr())
+                            else:
+                                print(m)
             input("Press Enter to continue...")
 
     asyncio.run(main())
